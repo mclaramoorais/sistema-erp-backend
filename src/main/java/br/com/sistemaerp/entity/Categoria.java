05d6a6ad156b084c4abdfb3a619620a23b6c37c2
Maria Clara De Morais <wamariaclara@hotmail.com>
2026-07-13 09:08:25 -0300

    Cria entidade Categoria para organização de produtos

---
+++ src/main/java/br/com/sistemaerp/entity/Categoria.java
@@ -0,0 +1,25 @@
+package br.com.sistemaerp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "categorias")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String nome;
+
+    @Column(length = 255)
+    private String descricao;
+
+}
