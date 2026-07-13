5f7d39c1b5b930f11e89c8deac71f9de4743dcae
Maria Clara De Morais <wamariaclara@hotmail.com>
2026-07-13 09:28:04 -0300

    Cria repository de Usuario para autenticação e gerenciamento de usuários

---
+++ src/main/java/br/com/sistemaerp/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package br.com.sistemaerp.repository;
+
+import br.com.sistemaerp.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+}
