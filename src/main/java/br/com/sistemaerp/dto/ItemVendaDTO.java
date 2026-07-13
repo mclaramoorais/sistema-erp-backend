7934c25be72d5c5fdb145e6d7c68906550f54ab2
Maria Clara De Morais <wamariaclara@hotmail.com>
2026-07-13 09:42:57 -0300

    Create ItemVendaDTO class for sales item data

---
+++ src/main/java/br/com/sistemaerp/dto/ItemVendaDTO.java
@@ -0,0 +1,26 @@
+package br.com.sistemaerp.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ItemVendaDTO {
+
+    private Long id;
+
+    @NotNull(message = "O produto é obrigatório")
+    private Long produtoId;
+
+    @NotNull(message = "A quantidade é obrigatória")
+    private Integer quantidade;
+
+    @NotNull(message = "O valor unitário é obrigatório")
+    private BigDecimal valorUnitario;
+
+}
