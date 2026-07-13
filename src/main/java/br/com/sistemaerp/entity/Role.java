dcbe78ee55d1687841f0e842c9f0e58c9f29fea2
Maria Clara De Morais <wamariaclara@hotmail.com>
2026-07-13 09:01:22 -0300

    Add Role entity with JPA annotations

---
+++ src/main/java/br/com/sistemaerp/entity/Role.java
@@ -0,0 +1,22 @@
+package br.com.sistemaerp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nome;
+
+}
