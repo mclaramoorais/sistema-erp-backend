ec4058956e681aec0c97181ac5682fb95229ef2a
Maria Clara De Morais <wamariaclara@hotmail.com>
2026-07-13 09:04:27 -0300

    Create Cliente.java

---
+++ src/main/java/br/com/sistemaerp/entity/Cliente.java
@@ -0,0 +1,40 @@
+package br.com.sistemaerp.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "clientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(unique = true, length = 20)
+    private String cpfCnpj;
+
+    @Column(length = 20)
+    private String telefone;
+
+    @Column(length = 150)
+    private String email;
+
+    @Column(length = 255)
+    private String endereco;
+
+    @Column(length = 100)
+    private String cidade;
+
+    @Column(length = 2)
+    private String estado;
+
+}
