3614cf26152ef3a1a04dd560e350a146a871b9d6
Maria Clara De Morais <wamariaclara@hotmail.com>
2026-07-13 09:36:38 -0300

    Create UsuarioDTO class with validation annotations

---
+++ src/main/java/br/com/sistemaerp/dto/UsuarioDTO.java
@@ -0,0 +1,32 @@
+package br.com.sistemaerp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Informe um e-mail válido")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    private String senha;
+
+    @NotNull(message = "O perfil de acesso é obrigatório")
+    private Long roleId;
+
+    private Boolean ativo;
+
+}
